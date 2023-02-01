d0a3eb32f55fb6c5dacc587159de62ba8280a095
mohammad <mo.se.12369@gmail.com>
2023-02-01 10:09:25 +0330

    Square Star Pattern

---
+++ Square Star Pattern/Square Star Pattern/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square_Star_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number : ");
+
+            string s = Console.ReadLine();
+            int a = Convert.ToInt32(s);
+
+            for (int i = 1; i <= a; i++)
+            {
+                for (int j = 1; j <= a; j++) Console.Write("*");
+                           
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
